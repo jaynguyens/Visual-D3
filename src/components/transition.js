ae13e72a47fac67482376b9ec4519ffa65b22ee3
nguyenami <iam.nguyen@pm.me>
2020-05-11 17:45:11 +0100

    animated rect

--- src/components/transition.js
+++ src/components/transition.js
@@ -5,8 +5,33 @@ const Transition = () => {
   const visualRef = useRef();
 
   useEffect(() => {
+    const margin = {
+      top: 50,
+      bottom: 50,
+      left: 50,
+      right: 50,
+    };
+    const width = 640 - margin.left - margin.right;
+    const height = 480 - margin.top - margin.bottom;
+
     // eslint-disable-next-line
-    const svg = d3.select(visualRef.current);
+    const svg = d3
+      .select(visualRef.current)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom);
+
+    svg
+      .append("rect")
+      .attr("x", 10)
+      .attr("y", 200)
+      .attr("width", 100)
+      .attr("height", 30)
+      .attr("fill", "#69b3a2")
+      .attr("stroke-width", 1)
+      //animation
+      .transition()
+      .duration(2000)
+      .attr("width", 400);
   }, []);
 
   return (
